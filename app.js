fba5181197dd1482721ab5df14e6e9284f67136f
Julia <holbraykh@gmail.com>
2020-02-26 15:10:11 +0200

    file storage, multer

--- app.js
+++ app.js
@@ -2,10 +2,31 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const multer = require('multer');
 const feedRoutes = require('./routes/feed');
 
 // configuration
 const app = express();
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req,file,cb) => {
+        cb(null, new Date().toISOString() + '-' + file.originalname);
+    }
+});
+const fileFilter = (req, file, cb) => {
+    if(
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
 app.use(bodyParser.json());
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use((req, res, next) => {
