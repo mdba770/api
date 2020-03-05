accc487cbb4bdcf093ff41feab5074d2cdb4f9f5
Julia <holbraykh@gmail.com>
2020-03-05 13:34:18 +0200

    get status, change status

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const {body} = require('express-validator');
 const authController = require('../controllers/auth');
 const User = require('../models/user');
 const router = express.Router();
+const isAuth = require('../middleware/is-auth');
 
 // GET /auth/signup
 router.put('/signup', [
@@ -34,5 +35,14 @@ router.put('/signup', [
 ], authController.signup);
 
 router.post('/login', authController.login);
+
+router.get('/status', isAuth, authController.getUserStatus);
+
+router.patch('/status', isAuth, [
+    body('status')
+        .trim()
+        .not()
+        .isEmpty()
+], authController.updateUserStatus);
 
 module.exports = router;
